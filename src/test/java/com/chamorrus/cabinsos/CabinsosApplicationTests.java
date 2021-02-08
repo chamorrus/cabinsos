2557eb7cb282b89445d701d973dfc21d05445d68
chamorrus <chamorrus@gmail.com>
2021-02-08 10:51:55 +0100

    First commit

---
+++ src/test/java/com/chamorrus/cabinsos/CabinsosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamorrus.cabinsos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinsosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
